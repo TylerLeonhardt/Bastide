7996aa5fea2e90038ddf908b19f3832b6f057055
Jonathan Warner <jaxbot@gmail.com>
2015-09-08 23:43:59 -0400

    Add form validation for signup

--- frontend/_src/assets/js/bastide/register.js
+++ frontend/_src/assets/js/bastide/register.js
@@ -1,11 +1,37 @@
 if (!window.Bastide) window.Bastide = {};
 window.Bastide.Registration = {
     signup: function() {
+        var nameInput = document.querySelector('#signUpModal .name'),
+            emailInput = document.querySelector('#signUpModal .email'),
+            schoolInput = document.querySelector('#signUpModal .school');
+
         var params = {
-            name: document.querySelector('#signUpModal .name').value,
-            email: document.querySelector('#signUpModal .email').value,
-            school: document.querySelector('#signUpModal .school').value,
+            name: nameInput.value,
+            email: emailInput.value,
+            school: schoolInput.value,
         };
+
+        var error = false;
+        if (!params.name) {
+            error = true;
+            nameInput.className += " invalid";
+        }
+        if (!params.email) {
+            error = true;
+            emailInput.className += " invalid";
+        }
+        if (!params.school) {
+            error = true;
+            schoolInput.className += " invalid";
+        }
+        var errorMessage = document.querySelector('#signUpModal .errors');
+        if (error) {
+            errorMessage.className += ' visible';
+            return false;
+        } else {
+            errorMessage.className = errorMessage.className.replace(/ visible/g, '');
+        }
+
         if (window.DEV)
             _handleSignupCompleted();
         else
