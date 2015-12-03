6756b51910e85b700a5e397f005e258657ff5631
Jonathan Warner <jaxbot@gmail.com>
2015-12-03 00:32:14 -0500

    Finish up confirmation

--- modules/confirmation/index.js
+++ modules/confirmation/index.js
@@ -9,6 +9,11 @@ var fs = require('fs');
 module.exports = function(app) {
   app.use(route.post('/api/confirm', confirm));
   app.use(route.get('/confirm/:user_id/:token', confirm_page));
+  app.use(route.get('/api/confirm/csv/:key', get_csv));
+
+  function _expired(result) {
+    return ((new Date).getTime() / 1000) > (result.available_since + 3600 * 24 * 3);
+  }
 
   function *confirm_page(id, token) {
     var results = yield db.query("SELECT * FROM `confirmations` WHERE id = " + db.escape(id) + " AND token = " + db.escape(token));
@@ -24,6 +29,8 @@ module.exports = function(app) {
       id: results[0].id,
       email: results[0].email,
       name: results[0].name,
+      expired: _expired(results[0]),
+      done: !!results[0].has_confirmed,
     });
   }
 
@@ -35,6 +42,10 @@ module.exports = function(app) {
     results = results[0];
 
     if (results.length > 0 && results[0].token == response.token) {
+      if (_expired(results[0])) {
+	this.body = { "status": "expired" };
+	return;
+      }
       yield db.query("UPDATE `confirmations` SET dietary = " + db.escape(response.dietary) + ", tshirt = " + db.escape(response.tshirt) + ", dietary_info = " + db.escape(response.dietary_info) + ", transportation = " + db.escape(response.transportation) + ", resume_url = " + db.escape(response.resume_url) + ", ip = " + db.escape(this.request.cfip) + ", has_confirmed = 1 WHERE id = " + db.escape(response.id) + " AND token = " + db.escape(response.token));
 
       if (response.resume) {
@@ -53,7 +64,7 @@ module.exports = function(app) {
 
   function *get_csv(key) {
     if (key === config.secret_key) {
-      var results = yield db.query("SELECT * FROM `signups`");
+      var results = yield db.query("SELECT * FROM `confirmations`");
       var csvdata = "";
 
       results[0].forEach(function(result) {
--- modules/confirmation/public/confirm.js
+++ modules/confirmation/public/confirm.js
@@ -1,29 +1,56 @@
+/* stop reading pls i had like 1 hour to make this */
+var _ = function(id) { return document.getElementById(id); };
+
 function updateDietary() {
   var display = "none";
-  if (document.getElementById("dietary-2").checked) {
+  if (_("dietary-2").checked) {
     display = "inherit";
   }
-  document.getElementById("dietary-explain").style.display = display;
+  _("dietary-explain").style.display = display;
 }
 window.onload = function() {
+  if (ALREADY_DONE) {
+    _("form-content").className = 'hidden';
+    _("already-done").className += ' visible';
+  } else if (EXPIRED) {
+    _("form-content").className = 'hidden';
+    _("expired").className += ' visible';
+  }
   updateDietary();
 
-  document.getElementById("submit-form").addEventListener("click", function() {
+  _("submit-form").addEventListener("click", function() {
+    if (!_("agree").checked) {
+      alert("Going old school and using an alert to point out that you need to agree to the code of conduct before confirming.");
+      return;
+    }
+
     var form = {
       id: window.USER_ID,
       email: window.USER_EMAIL,
       token: window.TOKEN,
-      tshirt: document.getElementById("tshirt").value,
-      dietary: document.getElementById("dietary-2").checked,
-      dietary_info: document.getElementById("dietary-explain").value,
-      transportation: document.getElementById("transport-2").checked,
-      resume_url: document.getElementById("url").value,
+      tshirt: _("tshirt").value,
+      dietary: _("dietary-2").checked,
+      dietary_info: _("dietary-explain").value,
+      transportation: _("transport-2").checked,
+      resume_url: _("url").value,
     };
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/confirm");
     xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function() {
+      _("loader").className = 'hidden';
+      try {
+        if (JSON.parse(xhr.responseText).status === 'yay') {
+          _("success").className += ' visible';
+          return;
+        }
+      } catch (e) {
+      }
+      alert("Something went wrong. Here's what the server said: " + xhr.responseText);
+      _("form-content").className = 'visible';
+    };
 
-    var files = document.getElementById("resume-file").files;
+    var files = _("resume-file").files;
     if (files.length > 0) {
       for (var i = 0, f; f = files[i]; i++) {
         var reader = new FileReader();
@@ -38,5 +65,7 @@ window.onload = function() {
     } else {
       xhr.send(JSON.stringify(form));
     }
+    _("form-content").className = 'hidden';
+    _("loader").className += ' visible';
   });
 }
